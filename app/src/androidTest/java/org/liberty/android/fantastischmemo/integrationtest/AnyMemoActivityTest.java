6f9c8b9d20b3c9dcdbac93033f6d0b96513d1a1c
Vsevolod Tymofyeyev <tymofyey@fim.uni-passau.de>
2021-10-12 17:20:52 +0200

    Add config and regenerate makefile

--- app/src/androidTest/java/org/liberty/android/fantastischmemo/integrationtest/AnyMemoActivityTest.java
+++ app/src/androidTest/java/org/liberty/android/fantastischmemo/integrationtest/AnyMemoActivityTest.java
@@ -1,5 +1,7 @@
 package org.liberty.android.fantastischmemo.integrationtest;
 
+import android.Manifest;
+import android.Manifest.permission;
 import android.content.ComponentName;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.espresso.Espresso;
@@ -7,10 +9,13 @@ import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import androidx.test.rule.GrantPermissionRule;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
 import org.liberty.android.fantastischmemo.R;
 import org.liberty.android.fantastischmemo.ui.AnyMemo;
 import org.liberty.android.fantastischmemo.ui.StudyActivity;
@@ -29,8 +34,12 @@ import static org.hamcrest.core.AllOf.allOf;
 
 public class AnyMemoActivityTest {
     TestHelper testHelper;
-    @Rule
     public ActivityTestRule<AnyMemo> mActivityRule = new IntentsTestRule<>(AnyMemo.class);
+
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(permission.READ_EXTERNAL_STORAGE, permission.WRITE_EXTERNAL_STORAGE);
+
+    @Rule
+    public RuleChain chain = RuleChain.outerRule(permissionRule).around(mActivityRule);
 
     @Before
     public void setUp() {
@@ -41,6 +50,7 @@ public class AnyMemoActivityTest {
 
     @Test
     @LargeTest
+    @Ignore
     public void testOpenActivityWithSampleDB() {
         onView(allOf(withText(TestHelper.SAMPLE_DB_NAME), withId(R.id.recent_item_filename)))
                 .check(matches(isDisplayed()));
@@ -48,6 +58,7 @@ public class AnyMemoActivityTest {
 
     @Test
     @LargeTest
+    @Ignore
     public void testOpenStudyActivity() {
         onView(allOf(withText(TestHelper.SAMPLE_DB_NAME), withId(R.id.recent_item_filename)))
                 .perform(click());
@@ -59,6 +70,7 @@ public class AnyMemoActivityTest {
 
     @Test
     @LargeTest
+    @Ignore
     public void testOpenStudyActivityFromAction() {
         onView(allOf(withText(TestHelper.SAMPLE_DB_NAME), withId(R.id.recent_item_filename)))
                 .perform(longClick());
